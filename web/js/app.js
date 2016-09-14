aff696866aab8783666ac4f667a1f3afdc04d113
vishmal <vishmalwkc@gmail.com>
2016-09-14 23:42:52 +0530

    delete confirmatio box

--- web/js/app.js
+++ web/js/app.js
@@ -14,21 +14,28 @@ app.controller('myCtrl', function($scope, $http) {
             }
         };
 
-        $http.post('http://localhost:8000/deleteContact', $scope.data, config)
-            .success(function (data, status, headers, config) {
-                $http.get("http://localhost:8000/listdata")
-                    .then(function(response) {
-                        $scope.contacts = response.data;
-                    }, function(response) {
-                        $scope.contacts = "Something went wrong";
-                    });
-            })
-            .error(function (data, status, header, config) {
-                $scope.ResponseDetails = "Data: " + data +
-                    "<hr />status: " + status +
-                    "<hr />headers: " + header +
-                    "<hr />config: " + config;
-            });
+        var x;
+        if (confirm('Are you sure?\nDo you want to delete this contact?\n Contact id : '+$scope.id) == true) {
+            $http.post('http://localhost:8000/deleteContact', $scope.data, config)
+                .success(function (data, status, headers, config) {
+                    $http.get("http://localhost:8000/listdata")
+                        .then(function(response) {
+                            $scope.contacts = response.data;
+                        }, function(response) {
+                            $scope.contacts = "Something went wrong";
+                        });
+                })
+                .error(function (data, status, header, config) {
+                    $scope.ResponseDetails = "Data: " + data +
+                        "<hr />status: " + status +
+                        "<hr />headers: " + header +
+                        "<hr />config: " + config;
+                });
+        } else {
+            x = "You pressed Cancel!";
+        }
+
+
     };
 
     $scope.hide = function (id) {
